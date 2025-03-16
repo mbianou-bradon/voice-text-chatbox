c62148257de912b817288c4645c4d12f367e1086
mbianou-bradon <mbianoubradon@gmail.com>
2025-03-16 09:12:45 +0100

    feat(app): updated file structure and UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Chat from "@/components/chat/Chat";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col min-h-screen items-center justify-center max-w-[800px] mx-auto overflow-hidden">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <header>
           <p>Welcome on Voice-Text Chatbox</p>
